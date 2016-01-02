dd90f86024408f2d0291cb98333b880604803e51
Ethan Neff <ethan.neff@eneff.com>
2016-01-01 17:12:28 -0800

    renamed completed to failure and success

---
+++ apps/improve/www/app/components/api/1/users/items/failure/failure.php
@@ -0,0 +1,28 @@
+<?php
+
+// libs
+require_once("../../../lib/Api.php");
+require_once("../../../lib/Tracking.php");
+
+// restrict
+Api::authenticate();
+
+// params
+$output = false;
+$data["user_id"] = Api::get_url_id("users");
+$data["item_id"] = (!empty($type)) ? Api::get_url_id($type) : Api::get_url_id("items");
+
+// actions
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+  // create
+  $output = Tracking::failure($data);
+}
+
+// output
+Api::error_check($output);
+Api::response($output);
+
+// test
+function test_failure() {
+  $_SERVER["REQUEST_METHOD"] = "POST";
+}

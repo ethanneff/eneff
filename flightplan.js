765e32b82dd847bac05c048bcf375e345288558c
Ethan Neff <ethan.neff@eneff.com>
2016-03-05 20:57:38 -0800

    updated flightplant

--- flightplan.js
+++ flightplan.js
@@ -13,12 +13,7 @@ plan.target('production', [
 
 // run commands on localhost
 plan.local(function(local) {
-  // uncomment these if you need to run a build on your machine first
-  // local.log('Run build');
-  // local.exec('gulp build');
-
   local.log('Copy files to remote hosts');
   var filesToCopy = local.exec('git ls-files', {silent: false});
-  // rsync files to all the destination's hosts
   local.transfer(filesToCopy, 'www');
 });

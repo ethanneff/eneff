564a7a10ae389fab4b7d1085672391d73eb5d894
Ethan Neff <ethan.neff@eneff.com>
2016-03-07 18:24:19 -0800

    updated more links from playground

--- apps/portfolio/www/app/components/playground/apps/js/angularjs_foundation/01_jquery_vs_angular_json/app.js
+++ apps/portfolio/www/app/components/playground/apps/js/angularjs_foundation/01_jquery_vs_angular_json/app.js
@@ -1,6 +1,6 @@
 angular.module('treehouseCourse', [])
   .controller('stageOneCtrl', function ($scope, $http) {
-    $http.get('people.json').success(function(people) {
+    $http.get('/apps/portfolio/www/app/components/playground/apps/js/angularjs_foundation/01_jquery_vs_angular_json/people.json/').success(function(people) {
       $scope.people = people;
     });
   });

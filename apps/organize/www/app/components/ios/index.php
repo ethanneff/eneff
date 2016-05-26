d3cefa5f3c93ef86b1696a150a2e825b41da9832
Ethan Neff <ethan.neff@eneff.com>
2016-05-26 14:05:14 -0700

    updated landing page for deep links on organize

---
+++ apps/organize/www/app/components/ios/index.php
@@ -0,0 +1,3 @@
+the landing page if trying to access a <a href="https://yp4ut.app.goo.gl/I2ST" title="dynamiclink">deep link</a> on a computer.
+
+will redirect to the app or iTunes if on an iOS device

fbdea917a60a6193ae86bdbbe2347a1f84d7885c
Ethan Neff <ethan.neff@eneff.com>
2016-05-26 14:55:50 -0700

    reworded the app store link to organize

--- apps/portfolio/www/app/components/playground/data.php
+++ apps/portfolio/www/app/components/playground/data.php
@@ -16,7 +16,7 @@ $data = array(
     'An app to organize your thoughts and notes.',
     'Has custom swipe gestures to complete, collapse, indent, reorder, edit, and set reminders for your notes.',
     'A demo of the app can be found <a href="//youtu.be/xgEHHqGtfXU" title="link to YouTube demo of app">here</a>.',
-    'You can find the app <a href="//itunes.apple.com/app/?id=1116178818" title="link to the App on the iTunes App Store">here</a> here on the iTunes App Store.',
+    'You can find the app on the iTunes App Store <a href="//itunes.apple.com/app/?id=1116178818" title="link to the App on the iTunes App Store">here</a>.',
     ],
     ),
   array(

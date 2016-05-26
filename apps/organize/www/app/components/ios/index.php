54bbbad2883ccce5eb4ba9aa36e2659640ffab4f
Ethan Neff <ethan.neff@eneff.com>
2016-05-26 14:13:42 -0700

    made landing page pretty

--- apps/organize/www/app/components/ios/index.php
+++ apps/organize/www/app/components/ios/index.php
@@ -1,3 +1,16 @@
-the landing page if trying to access a <a href="https://yp4ut.app.goo.gl/I2ST" title="dynamiclink">deep link</a> on a computer.
-
-will redirect to the app or iTunes if on an iOS device
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Organize App Landing Page</title>
+  <style type="text/css" media="screen">
+  * { padding: 0; margin: 0; background-color: #FAFAFA; box-sizing: border-box; }
+  a { color: #3498db; text-decoration: none;}
+  p { color: #212121; text-align: center; font-size: 1.25em; padding-top: 0.6em;}
+  body { border-top: 0.4em solid #212121; }
+  </style>
+</head>
+<body>
+  <p>the landing page if trying to access a <a href="https://yp4ut.app.goo.gl/I2ST" title="dynamiclink">deep link</a> on a computer</p>
+  <p>the page will redirect to the App or iTunes Store if on an iOS device</p>
+</body>
+</html>

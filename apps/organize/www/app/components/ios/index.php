27a4a1ca2898ae41d18a41b17e1199b11d88b776
Ethan Neff <ethan.neff@eneff.com>
2016-06-09 16:30:16 -0700

    added redirect to organize ios landing page

--- apps/organize/www/app/components/ios/index.php
+++ apps/organize/www/app/components/ios/index.php
@@ -2,8 +2,6 @@
 <html>
 <head>
   <title>Organize App Landing Page</title>
-  <!-- redirect -->
-  <meta http-equiv="refresh" content="0; URL='https://itunes.apple.com/app/?id=1116178818'" />
   <style type="text/css" media="screen">
   * { padding: 0; margin: 0; background-color: #FAFAFA; box-sizing: border-box; }
   a { color: #3498db; text-decoration: none;}
@@ -12,7 +10,7 @@
   </style>
 </head>
 <body>
-  <p>the landing page if trying to access a deep link on a computer</p>
-  <p>the page will redirect to the <a href="https://yp4ut.app.goo.gl/I2ST" title="deep link">App</a> or <a href="https://itunes.apple.com/app/?id=1116178818" title="iTunes Store">iTunes Store</a> if on an iOS device</p>
+  <p>Sorry this <a href="https://yp4ut.app.goo.gl/I2ST" title="deep link">app</a> is only available for iPhones and iPads in the <a href="https://itunes.apple.com/app/?id=1116178818" title="iTunes Store">iTunes Store</a>.</p>
+  <p>Android and Desktop versions are coming soon.</p>
 </body>
 </html>

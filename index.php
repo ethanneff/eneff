18a62571fc16aa2204ea6a509ff0fef46ca89f80
Ethan Neff <ethan.neff@eneff.com>
2016-07-21 15:52:34 -0700

    trying to fix ssl

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 
-// // prevent click jacking
-// header("X-Frame-Options: DENY");
-// header("Content-Security-Policy: frame-ancestors 'none'", false);
+// prevent click jacking
+header("X-Frame-Options: DENY");
+header("Content-Security-Policy: frame-ancestors 'none'", false);
 
-// // main page
-// include_once("apps/portfolio/www/index.php");
-?>
-
-testing ssl
+// main page
+include_once("apps/portfolio/www/index.php");
